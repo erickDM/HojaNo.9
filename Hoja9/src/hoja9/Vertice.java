8db5d5be47f6f47e68d19983c42f59a4389addde
erickDM <erickdemata@gmail.com>
2014-11-10 22:47:54 -0600

    Se agregaron clases

---
+++ Hoja9/src/hoja9/Vertice.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja9;
+
+/**
+ *
+ * @author Luis José
+ */
+public class Vertice {
+    private String name;
+    public Vertice(String name){
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+   
+}
